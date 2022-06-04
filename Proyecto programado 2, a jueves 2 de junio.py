e451a5a1626c42e7ccc3b1030c5a5e1d6d5c5e55
rubenabarca <rudavid0400@gmail.com>
2022-06-03 18:14:48 -0600

    Update Proyecto programado 2, a jueves 2 de junio.py

--- Proyecto programado 2, a jueves 2 de junio.py
+++ Proyecto programado 2, a jueves 2 de junio.py
@@ -1,6 +1,7 @@
 # Proyecto programado 2, a sábado 28 de mayo
 # Rubén David Abarca Ramírez - 2022017072
 # Saimon Hernández Venegas - 2022090508
+# Daniel Rubén Arce Madriz
 
 
 from asyncio.windows_events import NULL
